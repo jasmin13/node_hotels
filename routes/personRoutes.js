b8453f5ff0e2b4e2b21ee2159d5c30d2f00a66a0
jasmin13 <jasmin.tatmiya@gmail.com>
2024-07-03 18:01:10 +0530

    Fix Error

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Person = require("./../models/person");
+const Person = require("./../models/Person");
 
 // Post routes to add person
 router.post("/", async (req, res) => {
